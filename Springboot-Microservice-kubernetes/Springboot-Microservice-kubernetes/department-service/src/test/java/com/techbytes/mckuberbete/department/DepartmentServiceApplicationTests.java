1a63290bb6b01a2ef78e07f7f5bef8d96fadea1b
anand nerurkar <anand.nerurkar@gmail.com>
2022-07-14 18:25:51 +0530

    updated

--- Springboot-Microservice-kubernetes/Springboot-Microservice-kubernetes/department-service/src/test/java/com/techbytes/mckuberbete/department/DepartmentServiceApplicationTests.java
+++ Springboot-Microservice-kubernetes/Springboot-Microservice-kubernetes/department-service/src/test/java/com/techbytes/mckuberbete/department/DepartmentServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.dailycodebuffer.department;
+package com.techbytes.mckuberbete.department;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
